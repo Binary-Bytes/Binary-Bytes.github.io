ff6a800cc191f408747f62600df54389484bf113
Binary Bytes <binarybytes0011@gmail.com>
2021-09-22 14:54:54 +0530

    Remove Message Me

--- src/js/script.js
+++ src/js/script.js
@@ -10,20 +10,3 @@ function toggleMenu() {
     menu.classList.toggle('active')
     menuToggle.classList.toggle('active')
 }
-
-function sendMessage() {
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let message = document.getElementById('message').value;
-
-    console.log(name, email, message)
-
-    if (name !== null && name !== '' && email !== null && email !== '' && message !== null && message !== '') {
-        window.open('https://wa.me/9999108282/?text=' + name + ' (' + email + ') : ' + message);
-    } else {
-        alert('Fill All The Details Before Sending The Message !!');
-
-        document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
-    }
-}
