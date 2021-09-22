d76308ecb0a027b92eca6b6bd54dde01d057967f
Binary Bytes <binarybytes0011@gmail.com>
2021-09-22 14:43:24 +0530

    Fix Contact Me Bug

--- src/js/script.js
+++ src/js/script.js
@@ -12,9 +12,11 @@ function toggleMenu() {
 }
 
 function sendMessage() {
-    let name = document.getElementById('#name');
-    let email = document.getElementById('#email');
-    let message = document.getElementById('#message');
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    console.log(name, email, message)
 
     if (name !== null && name !== '' && email !== null && email !== '' && message !== null && message !== '') {
         window.open('https://wa.me/9999108282/?text=' + name + ' (' + email + ') : ' + message);
